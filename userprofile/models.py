7eae8270645330cb91df6e1e6478c070a1181d88
Muhammet Oktay <muhammetokty@gmail.com>
2024-06-09 20:10:09 +0300

    UserProfile Backend init

--- userprofile/models.py
+++ userprofile/models.py
@@ -5,10 +5,10 @@ from rest_framework.authtoken.admin import User
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(max_length=500, blank=True)
-    profile_picture = models.ImageField(upload_to='profile_pics', null =True, blank=True)
+    profile_picture = models.ImageField(upload_to='profile_pics', null=True, blank=True)
 
     @classmethod
-    def create(cls,user):
+    def create(cls, user):
         user_profile = cls(user=user)
         user_profile.save()
         return user_profile
