8567eb5de79bd57ea2c079c35a575ab967eac4aa
Burak Aksoy <amphipron.percula@gmail.com>
2024-06-10 20:34:59 +0300

    updated for bug fixes

--- chat/models.py
+++ chat/models.py
@@ -1,16 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+from django.utils import timezone
+
 
 class Chatroom(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     creator = models.ForeignKey(User, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(default=timezone.now)  # Manually define a default value
     updated_at = models.DateTimeField(auto_now=True)
 
-    def str(self):
+    def __str__(self):
         return self.name
+
 
 
 class Message(models.Model):
