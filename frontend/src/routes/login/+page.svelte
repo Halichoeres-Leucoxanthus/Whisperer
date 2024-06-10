4ba530f69d49346c792bcf9b8ebdc6f677027dee
Burak Aksoy <amphipron.percula@gmail.com>
2024-06-10 14:23:28 +0300

    updated

--- frontend/src/routes/login/+page.svelte
+++ frontend/src/routes/login/+page.svelte
@@ -1,62 +1,61 @@
 <script>
-import {goto} from "$app/navigation";
-import { onMount, setContext } from 'svelte';
-import Header from '../../components/Header.svelte';
+    import { goto } from "$app/navigation";
+    import { onMount, setContext } from 'svelte';
+    import Header from '../../components/Header.svelte';
 
-let username = '';
-let email = '';
-let password = '';
-let errors = {};
-let isAuthenticated = false;
+    let username = '';
+    let email = '';
+    let password = '';
+    let errors = {};
 
-onMount(async () => {
-    try {
-        const response = await fetch('http://localhost:8000/check-login/', {
-            credentials: 'include'
-        });
-        const data = await response.json();
-        isAuthenticated = data.message === 'User is logged in';
+    onMount(async () => {
+        try {
+            const response = await fetch('http://localhost:8000/check-login/', {
+                credentials: 'include'
+            });
+            const data = await response.json();
+            const isAuthenticated = data.message === 'User is logged in';
 
-        // Redirect if user is authenticated
-        if (isAuthenticated) {
-            await goto('/'); // Redirect to the dashboard or any other page
+            // Redirect if user is authenticated
+            if (isAuthenticated) {
+                await goto('/'); // Redirect to the dashboard or any other page
+            }
+        } catch (error) {
+            console.error('Error checking login status:', error);
         }
-    } catch (error) {
-        console.error('Error checking login status:', error);
-    }
-});
-let handleSubmit = async () => {
-    try {
-        const endpoint = 'http://localhost:8000/login/';
-        const requestOptions = {
-            method: "POST",
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ username: username, email: email, password: password })
-        };
+    });
 
-        const response = await fetch(endpoint, requestOptions);
-        const data = await response.json();
+    let handleSubmit = async () => {
+        try {
+            const endpoint = 'http://localhost:8000/login/';
+            const requestOptions = {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ username: username, email: email, password: password })
+            };
 
-        if (response.status === 200) {
-            // Set the CSRF token and session ID in the cookies
-            document.cookie = `csrftoken=${data.csrfToken}; path=/`;
-            document.cookie = `sessionid=${data.sessionId}; path=/`;
+            const response = await fetch(endpoint, requestOptions);
+            const data = await response.json();
 
-            // Set the session ID in the context
-            setContext('sessionId', data.sessionId);
+            if (response.status === 200) {
+                // Set the CSRF token and session ID in the cookies
+                document.cookie = `csrftoken=${data.csrfToken}; path=/`;
+                document.cookie = `sessionid=${data.sessionId}; path=/`;
 
-            await goto('/');
-        } else {
-            errors = data.body;
-            console.log(data);
+                // Set the session ID in the context
+                setContext('sessionId', data.sessionId);
+
+                await goto('/');
+            } else {
+                errors = data.body;
+                console.log(data);
+            }
+        } catch (error) {
+            console.error('Error:', error);
         }
-    } catch (error) {
-        console.error('Error:', error);
-    }
-};
-
+    };
 </script>
 
 <Header />
