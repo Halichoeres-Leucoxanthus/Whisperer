fcf10648a804601b420bd8383c8922918b38ea18
Muhammet Oktay <muhammetokty@gmail.com>
2024-06-10 21:40:23 +0300

    chatroom models

--- chat/models.py
+++ chat/models.py
@@ -3,17 +3,19 @@ from django.contrib.auth.models import User
 
 from django.utils import timezone
 
-
 class Chatroom(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     creator = models.ForeignKey(User, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(default=timezone.now)  # Manually define a default value
+    created_at = models.DateTimeField(default=timezone.now)
     updated_at = models.DateTimeField(auto_now=True)
+    type = models.CharField(max_length=10, choices=['private', 'global'])
+    users = models.ManyToManyField(User, related_name='chatrooms', blank=True)
 
-    def __str__(self):
+
+
+    def str(self):
         return self.name
-
 
 class Message(models.Model):
     chatroom = models.ForeignKey(Chatroom, on_delete=models.CASCADE)
@@ -21,5 +23,5 @@ class Message(models.Model):
     text = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
+    def str(self):
         return f"Message in {self.chatroom.name} by {self.user.username}"
